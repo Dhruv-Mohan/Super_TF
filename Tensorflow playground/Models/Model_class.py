42a9edbf6cc42039e9267e3acfc174895c8d83cc
Dhruv Mohan <Dhruv.Mohan@outlook.com>
2017-07-19 18:51:52 +0530

    Addding training and restoring to Model_class

--- Tensorflow playground/Models/Model_class.py
+++ Tensorflow playground/Models/Model_class.py
@@ -7,6 +7,9 @@ class Model_class(object):
     def __init__(self, **kwargs):
         self.Model_name = kwargs['Model_name']
         self.kwargs = kwargs
+        self.global_step=tf.Variable(0,trainable=False,dtype=tf.int32,name='global_step')
+
+
 
 
     def Set_loss(self, params):
@@ -17,7 +20,74 @@ class Model_class(object):
 
     def Set_optimizer(self, params):
         self.optimizer = params #Const optimizer params 
-        self.train_step = self.optimizer.minimize(self.loss)
+        self.train_step = self.optimizer.minimize(self.loss,global_step=self.global_step)
+
 
     def Construct_Model(self):
         [self.input_placeholder, self.output_placeholder, self.output] = Factory(**self.kwargs).get_model()
+
+
+    def Construct_Accuracy_op(self):
+        with tf.name_scope('accuracy'):
+            with tf.name_scope('correct_prediction'):
+                correct_prediction = tf.equal(tf.argmax(self.output, 1), tf.argmax(self.output_placeholder, 1))
+            with tf.name_scope('accuracy'):
+                self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+    def Construct_Writers(self,session):
+        self.saver = tf.train.Saver()
+        if self.kwargs['Summary']:
+            self.train_writer = tf.summary.FileWriter(self.kwargs['Save_dir'] + '/train/', session.graph)
+            self.test_writer = tf.summary.FileWriter(self.kwargs['Save_dir'] + '/test/')
+        else:
+            graph_writer = tf.summary.FileWriter(self.kwargs['Save_dir'] + '/train/')
+            graph_writer.add_graph(session.graph)
+            graph_writer.close();
+
+
+    def Try_restore(self,session):
+        latest_ckpt = tf.train.latest_checkpoint(self.kwargs['Save_dir'] + '/mdl/')
+        if latest_ckpt:
+            print('Ckpt_found')
+            print(latest_ckpt)
+            self.saver.restore(session,latest_ckpt)
+        else:
+            print('Ckpt_not_found')
+
+
+    def Train_Iter(self, session, iterations, data , restore=True):
+
+        #Try restore
+        if restore:
+            self.Try_restore(session)
+
+        if self.kwargs['Summary']:
+            self.merged = tf.summary.merge_all()
+
+        for step in range(iterations):
+            batch = data.next_batch(self.kwargs['Batch_size'])
+            if(step + 1) % 100 == 0:
+                #enter test and saver block
+                #add saving op
+                self.saver.save(session, self.kwargs['Save_dir'] + '/mdl/lenet.ckpt',global_step=self.global_step)
+                if self.kwargs['Summary']:
+                    summary, train_accuracy = session.run([self.merged, self.accuracy], \
+                        feed_dict={self.input_placeholder: batch[0], self.output_placeholder: batch[1]})
+                    self.test_writer.add_summary(summary, step)
+                else:
+                    train_accuracy = session.run([self.accuracy], \
+                        feed_dict={self.input_placeholder: batch[0], self.output_placeholder: batch[1]})
+
+                print(train_accuracy)
+            print('step %d' %(step))
+
+            #Train Step
+            if self.kwargs['Summary']:
+                summary,  _ = session.run([self.merged, self.train_step], \
+                    feed_dict={self.input_placeholder: batch[0], self.output_placeholder: batch[1]})
+                self.train_writer.add_summary(summary, step)
+            else:
+                _ = session.run([self.train_step], \
+                    feed_dict={self.input_placeholder: batch[0], self.output_placeholder: batch[1]})
+
--- Tensorflow playground/Tensorflow_playground.py
+++ Tensorflow playground/Tensorflow_playground.py
@@ -12,6 +12,9 @@ _IMAGE_CSPACE_      = 1
 _CLASSES_           = 10
 _MODEL_NAME_        = 'Lenet'
 _ITERATIONS_        = 200
+_LEARNING_RATE_     = 1e-4
+_SAVE_DIR_          = 'E:/Ttest'
+
 
 def main():
     ''' Main function'''
@@ -22,7 +25,7 @@ def main():
 
     #Construct model
     Simple_DNN = Model_class(Model_name=_MODEL_NAME_, Summary=_SUMMARY_, \
-        Batch_size=_BATCH_SIZE_, Image_width=_IMAGE_WIDTH_, Image_height=_IMAGE_HEIGHT_, Image_cspace=_IMAGE_CSPACE_, Classes=_CLASSES_)
+        Batch_size=_BATCH_SIZE_, Image_width=_IMAGE_WIDTH_, Image_height=_IMAGE_HEIGHT_, Image_cspace=_IMAGE_CSPACE_, Classes=_CLASSES_, Save_dir=_SAVE_DIR_)
 
     Simple_DNN.Construct_Model()
 
@@ -36,64 +39,16 @@ def main():
 
     #Set optimizer
     with tf.name_scope('Train'):
-        Simple_DNN.Set_optimizer(tf.train.AdamOptimizer(1e-4))
+        Simple_DNN.Set_optimizer(tf.train.AdamOptimizer(_LEARNING_RATE_))
 
+    #Construct op to check accuracy
+    Simple_DNN.Construct_Accuracy_op()
 
-
-    #Op to check accuracy
-    with tf.name_scope('accuracy'):
-        with tf.name_scope('correct_prediction'):
-            correct_prediction = tf.equal(tf.argmax(Simple_DNN.output, 1), tf.argmax(Simple_DNN.output_placeholder, 1))
-
-        with tf.name_scope('accuracy'):
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-
-    #Train and save
-    if _SUMMARY_:
-        merged = tf.summary.merge_all()
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
-
-    #Construct writers if summary
-        if _SUMMARY_:
-            train_writer = tf.summary.FileWriter('E:/Ttest/train', sess.graph)
-            test_writer = tf.summary.FileWriter('E:/Ttest/test')
-        else:
-            graph_writer = tf.summary.FileWriter('E:/Ttest/train')
-            graph_writer.add_graph(sess.graph)
-            graph_writer.close();
-    #Init global vars
-    sess.run(tf.global_variables_initializer())
-    for i in range(_ITERATIONS_):
-        batch = mnist.train.next_batch(_BATCH_SIZE_)
-        if i % 100 == 0:
-
-            #Save ckpt
-            saver.save(sess, "E:/Ttest/mdl/lenet.ckpt", i)
-
-            #Calculate accuracy
-            if _SUMMARY_:
-                summary, train_accuracy = sess.run([merged, accuracy], \
-                    feed_dict={Simple_DNN.input_placeholder: batch[0], Simple_DNN.output_placeholder: batch[1]})
-                test_writer.add_summary(summary, i)
-            else:
-                train_accuracy = sess.run([accuracy], \
-                    feed_dict={Simple_DNN.input_placeholder: batch[0], Simple_DNN.output_placeholder: batch[1]})
-
-            print(train_accuracy)
-        print('step %d' %(i))
-
-        #Train Step
-        if _SUMMARY_:
-            summary, _ = sess.run([merged, Simple_DNN.train_step], \
-                feed_dict={Simple_DNN.input_placeholder: batch[0], Simple_DNN.output_placeholder: batch[1]})
-            train_writer.add_summary(summary, i)
-        else:
-            _ = sess.run([Simple_DNN.train_step], \
-                feed_dict={Simple_DNN.input_placeholder: batch[0], Simple_DNN.output_placeholder: batch[1]})
-
-
+    #Training block
+    with tf.Session() as session:
+        session.run(tf.global_variables_initializer())
+        Simple_DNN.Construct_Writers(session)
+        Simple_DNN.Train_Iter(session, _ITERATIONS_, mnist.train, restore=False)
 
 
 
