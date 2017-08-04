d28c5079c7887aa30d878b7d31b2078106224415
Dhruv Mohan <Dhruv.Mohan@outlook.com>
2017-08-05 04:07:46 +0530

    Added ignored files

---
+++ Tensorflow playground/Dataset_IO/Dataset_conifg_classification.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+class Dataset_conifg_classification(object):
+    """description of class"""
+
+    def __init__(self):
+        self._Image_handle     = 'image_raw'
+        self._Label_handle     = 'label'
+        self._Height_handle    = 'height'
+        self._Width_handle     = 'width'
+        self._Depth_handle     = 'depth'
+        self._Feature_dict     =  {\
+            self._Label_handle  : tf.FixedLenFeature([], tf.int64),\
+            self._Image_handle  : tf.FixedLenFeature([], tf.string),\
+            self._Height_handle : tf.FixedLenFeature([], tf.int64),\
+            self._Width_handle  : tf.FixedLenFeature([], tf.int64),\
+            self._Depth_handle  : tf.FixedLenFeature([], tf.int64)}
