93c8d951da4e47f5b1c602c88c676647f14d08cf
Dhruv Mohan <Dhruv.Mohan@outlook.com>
2017-08-14 01:56:23 +0530

    Added Alexnet example and VGG16

---
+++ Tensorflow playground/Examples/Alexnet.py
@@ -0,0 +1,72 @@
+from Models.Model_class import Model_class
+import tensorflow as tf
+from Dataset_IO.Dataset_reader_classification import Dataset_reader_classification
+_SUMMARY_           = True
+_BATCH_SIZE_        = 48
+_IMAGE_WIDTH_       = 299
+_IMAGE_HEIGHT_      = 299
+_IMAGE_CSPACE_      = 1
+_CLASSES_           = 18
+_MODEL_NAME_        = 'Alexnet'
+_ITERATIONS_        = 500000
+_LEARNING_RATE_     = 0.0001
+_SAVE_DIR_          = 'E:/Ttest'
+_TEST_INTERVAL_     = 1000
+_RESTORE_           = True
+_DATASET_PATH_      = 'E:\\Alexnet_train.tfrecords'
+
+
+def writer_pre_proc(images):
+    print('adding to graph')
+    resized_images = tf.image.resize_images(images, size=[_IMAGE_HEIGHT_,_IMAGE_WIDTH_])
+    rgb_image_float = tf.image.convert_image_dtype(resized_images, tf.float32) 
+    reshaped_images = tf.reshape(rgb_image_float,[-1,_IMAGE_HEIGHT_*_IMAGE_WIDTH_*_IMAGE_CSPACE_])
+    return reshaped_images
+
+
+
+def main():
+    dummy_reader = Dataset_reader_classification(filename=_DATASET_PATH_, image_shape=[_IMAGE_HEIGHT_, _IMAGE_WIDTH_, _IMAGE_CSPACE_], num_classes=_CLASSES_)
+    dummy_reader.pre_process_image(writer_pre_proc)
+
+    with tf.name_scope('AlexNet'):
+        Alexnet = Model_class(Model_name=_MODEL_NAME_, Summary=_SUMMARY_, \
+            Batch_size=_BATCH_SIZE_, Image_width=_IMAGE_WIDTH_, Image_height=_IMAGE_HEIGHT_, Image_cspace=_IMAGE_CSPACE_, Classes=_CLASSES_, Save_dir=_SAVE_DIR_)
+
+        Alexnet.Construct_Model()
+
+
+    #Set loss
+        with tf.name_scope('Cross_entropy'):
+            Alexnet.Set_loss(tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=Alexnet.model_dict['Output_ph'], logits=Alexnet.model_dict['Output'])))
+
+
+
+    #Set optimizer
+        with tf.name_scope('Train'):
+            Alexnet.Set_optimizer(tf.train.AdamOptimizer(_LEARNING_RATE_))
+
+        #Construct op to check accuracy
+        Alexnet.Construct_Accuracy_op()
+        Alexnet.Construct_Predict_op()
+
+    #Setting test and train placeholders 
+    with tf.name_scope('Control Params'):
+        Alexnet.Set_test_control({'Dropout_prob_ph': 1})
+        Alexnet.Set_train_control({'Dropout_prob_ph': 0.5})
+
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth=True
+
+    #Training block
+    with tf.Session(config=config) as session:
+        session.run(tf.global_variables_initializer())
+        Alexnet.Construct_Writers()
+        Alexnet.Train_Iter(iterations=_ITERATIONS_, test_iterations=_TEST_INTERVAL_, data=dummy_reader, restore=_RESTORE_)
+        Predictions = Alexnet.Predict(session,mnist.train.next_batch(10)[0])
+
+
+if __name__ == "__main__":
+    main()
--- Tensorflow playground/Model_builder/Build_factory.py
+++ Tensorflow playground/Model_builder/Build_factory.py
@@ -9,6 +9,59 @@ class Factory(object):
         #return self.Build_Lenet()
         print('Build_'+self.model_name+'()')
         return (eval('self.Build_'+self.model_name+'()'))
+    def Build_vgg16(self):
+        with tf.name_scope('Vgg_model'):
+            with Builder(**self.kwargs) as vgg16_builder:
+                input_placeholder = tf.placeholder(tf.float32, \
+                    shape=[None, self.kwargs['Image_width']*self.kwargs['Image_height']*self.kwargs['Image_cspace']], name='Input')
+                output_placeholder = tf.placeholder(tf.float32, shape=[None, self.kwargs['Classes']], name='Output')
+                dropout_prob_placeholder = tf.placeholder(tf.float32, name='Dropout')
+                input_reshape = alexnet_builder.Reshape_input(input_placeholder, width=self.kwargs['Image_width'], height=self.kwargs['Image_height'], colorspace= self.kwargs['Image_cspace'])
+
+                #FEATURE EXTRACTION
+                conv1a = vgg16_builder.Conv2d_layer(input_reshape, filters=64)
+                conv1b = vgg16_builder.Conv2d_layer(conv1a, filters=64)
+
+                pool1 = vgg16_builder.Pool_layer(conv1b)
+
+                conv2a = vgg16_builder.Conv2d_layer(pool1, filters=128)
+                conv2b = vgg16_builder.Conv2d_layer(conv2a, filters=128)
+
+                pool2 = vgg16_builder.Pool_layer(conv2b)
+
+                conv3a = vgg16_builder.Conv2d_layer(pool2, filters=256)
+                conv3b = vgg16_builder.Conv2d_layer(conv3a, filters=256)
+                conv3c = vgg16_builder.Conv2d_layer(conv3b, filters=256)
+
+                pool3 = vgg16_builder.Pool_layer(conv3c)
+
+                conv4a = vgg16_builder.Conv2d_layer(pool3, filters=512)
+                conv4b = vgg16_builder.Conv2d_layer(conv4a, filters=512)
+                conv4c = vgg16_builder.Conv2d_layer(conv4b, filters=512)
+
+                pool4 = vgg16_builder.Pool_layer(conv4c)
+
+                conv5a = vgg16_builder.Conv2d_layer(pool4, filters=512)
+                conv5b = vgg16_builder.Conv2d_layer(conv5a, filters=512)
+                conv5c = vgg16_builder.Conv2d_layer(conv5b, filters=512)
+
+                pool5 = vgg16_builder.Pool_layer(conv5c)
+
+                #DENSELY CONNECTED
+                fc1 = vgg16_builder.FC_layer(pool5, filters=4096)
+                drop1 = vgg16_builder.Dropout_layer(fc1, dropout_prob_placeholder)
+
+                fc2 = vgg16_builder.FC_layer(drop1, filters=4096)
+                drop2 = vgg16_builder.Dropout_layer(fc2, dropout_prob_placeholder)
+
+                output = vgg16_builder.FC_layer(drop2, filters=self.kwargs['Classes'], readout=True)
+
+                VGG_dict = {'Input_ph': input_placeholder, 'Output_ph': output_placeholder, 'Output': output, 'Dropout_prob_ph': dropout_prob_placeholder }
+                return(VGG_dict)
+
+
+
+
 
     def Build_Alexnet(self):
         with tf.name_scope('Alexnet_model'):
