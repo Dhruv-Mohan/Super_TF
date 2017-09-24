2eaa4ed32787e31e9a85126f455d71a92370d488
Dhruv Mohan <Dhruv.Mohan@outlook.com>
2017-09-24 18:12:51 +0530

    updated unpooling layer

--- Tensorflow playground/utils/builder.py
+++ Tensorflow playground/utils/builder.py
@@ -103,41 +103,39 @@ class Builder(object):
                 Pool = tf.nn.avg_pool(input, ksize=k_size, \
                     strides=stride, padding=padding, name="AVG_POOL")
             elif pooling_type == 'MAXIND':
-                Pool, ind = tf.nn.max_pool_with_argmax(input, k_size=k_size,\
+                Pool, ind = tf.nn.max_pool_with_argmax(input, ksize=k_size,\
                     strides=stride, padding=padding, name='MAX_POOL_WITH_ARGMAX')
                 return Pool, ind
             #if self.Summary:
                 #tf.summary.histogram('Pool_activations', Pool)
             return Pool
 
-    def Unpool_layer(pool, ind, ksize=[1, 2, 2, 1], scope='unpool'):
+    def Unpool_layer(self, pool, ind, k_size=[1, 2, 2, 1]):
         # https://github.com/tensorflow/tensorflow/issues/2169
         """
            Unpooling layer after max_pool_with_argmax.
            Args:
                pool:   max pooled output tensor
                ind:      argmax indices
-               ksize:     ksize is the same as for the pool
+               k_size:     k_size is the same as for the pool
            Return:
                unpool:    unpooling tensor
         """
-        with tf.variable_scope(scope):
+        with tf.variable_scope('Unpool'):
             input_shape =  tf.shape(pool)
-            output_shape = [input_shape[0], input_shape[1] * ksize[1], input_shape[2] * ksize[2], input_shape[3]]
+            input_shape_aslist = pool.get_shape().as_list()
+            output_shape = tf.stack([input_shape[0], input_shape[1] * k_size[1], input_shape[2] * k_size[2], input_shape[3]])
+            output_shapeaslist = [-1,  input_shape_aslist[1]* k_size[1] ,  input_shape_aslist[2]  * k_size[2], input_shape_aslist[3]]
 
-            flat_input_size = tf.cumprod(input_shape)[-1]
-            flat_output_shape = tf.stack([output_shape[0], output_shape[1] * output_shape[2] * output_shape[3]])
-
-            pool_ = tf.reshape(pool, tf.stack([flat_input_size]))
-            batch_range = tf.reshape(tf.range(tf.cast(output_shape[0], tf.int64), dtype=ind.dtype), 
+            pool_ = tf.reshape(pool, [input_shape_aslist[1] * input_shape_aslist[2] * input_shape_aslist[3]])
+            batch_range = tf.reshape(tf.range(tf.cast(input_shape[0], tf.int64), dtype=ind.dtype), 
                                               shape=tf.stack([input_shape[0], 1, 1, 1]))
             b = tf.ones_like(ind) * batch_range
-            b = tf.reshape(b, tf.stack([flat_input_size, 1]))
-            ind_ = tf.reshape(ind, tf.stack([flat_input_size, 1]))
+            b = tf.reshape(b, tf.stack([ input_shape_aslist[1] * input_shape_aslist[2] * input_shape_aslist[3], 1]))
+            ind_ = tf.reshape(ind, tf.stack( [input_shape_aslist[1] * input_shape_aslist[2] * input_shape_aslist[3], 1]))
             ind_ = tf.concat([b, ind_], 1)
-
-            ret = tf.scatter_nd(ind_, pool_, shape=tf.cast(flat_output_shape, tf.int64))
-            ret = tf.reshape(ret, tf.stack(output_shape))
+            ret = tf.scatter_nd(ind_, pool_, shape=tf.cast([-1, output_shapeaslist[1] * output_shapeaslist[2] * output_shapeaslist[3] ], tf.int64))
+            ret = tf.reshape(ret, [-1, output_shapeaslist[1], output_shapeaslist[2], output_shapeaslist[3]])
             return ret
     def FC_layer(self, input, filters=1024, readout=False, weight_decay=0.00004): #Expects flattened layer
         with tf.name_scope('FC') as scope:
