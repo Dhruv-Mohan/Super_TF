41ab248af5ed1b1a0813d9132b1b284e4d4a8706
Dhruv Mohan <Dhruv.Mohan@outlook.com>
2017-10-14 04:13:02 +0530

    Added code to load all model architectures into build factory

--- Tensorflow playground/Model_builder/Build_factory.py
+++ Tensorflow playground/Model_builder/Build_factory.py
@@ -1,14 +1,29 @@
 from utils.builder import Builder
 import tensorflow as tf
-from Model_builder.Architecture.Classification import *
-from Model_builder.Architecture.Segmentation import * 
+import os
+
+Segnets = os.path.dirname(os.path.realpath(__file__)) +'\Architecture\Segmentation'
+Classnets = os.path.dirname(os.path.realpath(__file__)) +'\Architecture\Classification'
+
+segnet_archs = os.listdir(Segnets)
+classnet_archs = os.listdir(Classnets)
+
+for segnet, classnet in zip(segnet_archs, classnet_archs):
+    if ".pyc" not in segnet and "__init__" not in segnet and ".py" in segnet:
+        exec("from Model_builder.Architecture.Segmentation." + segnet[:-3] + " import *" )
+
+    if ".pyc" not in  classnet and "__init__" not in  classnet and ".py" in  classnet:
+        exec("from Model_builder.Architecture.Classification." + classnet[:-3] + " import *" )
+
+
 class Factory(object):
     """Factory class to build DNN Architectures"""
     #Look into adding a datastructure to keep track of last layer added to the graph
 
     def get_model(self):
+
         print('Building ' + self.model_name+'()')
-        return (eval(self.model_name + '.Build_' + self.model_name+'()'))
+        return (eval('Build_' + self.model_name+'()'))
 
     def __init__(self, **kwargs):
         #TODO: WRITE ERROR HANDLER AND PARSER 
