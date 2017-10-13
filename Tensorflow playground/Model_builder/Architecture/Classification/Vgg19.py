50ea03170d2e73c396ac01e9dcf1b72209ba951a
Dhruv Mohan <Dhruv.Mohan@outlook.com>
2017-10-14 00:30:53 +0530

    Split Vgg19

---
+++ Tensorflow playground/Model_builder/Architecture/Classification/Vgg19.py
@@ -0,0 +1,65 @@
+from utils.builder import Builder
+import tensorflow as tf
+
+
+
+def Build_vgg19(self):
+    '''Add paper and brief description'''
+        with tf.name_scope('Vgg_model'):
+            with Builder(**self.kwargs) as vgg19_builder:
+                input_placeholder = tf.placeholder(tf.float32, \
+                    shape=[None, self.kwargs['Image_width']*self.kwargs['Image_height']*self.kwargs['Image_cspace']], name='Input')
+                output_placeholder = tf.placeholder(tf.float32, shape=[None, self.kwargs['Classes']], name='Output')
+                dropout_prob_placeholder = tf.placeholder(tf.float32, name='Dropout')
+                input_reshape = vgg19_builder.Reshape_input(input_placeholder, width=self.kwargs['Image_width'], height=self.kwargs['Image_height'], colorspace= self.kwargs['Image_cspace'])
+
+                #Setting control params
+                vgg19_builder.control_params(Dropout_control=dropout_prob_placeholder)
+
+                #FEATURE EXTRACTION
+                conv1a = vgg19_builder.Conv2d_layer(input_reshape, filters=64)
+                conv1b = vgg19_builder.Conv2d_layer(conv1a, filters=64)
+
+                pool1 = vgg19_builder.Pool_layer(conv1b)
+
+                conv2a = vgg19_builder.Conv2d_layer(pool1, filters=128)
+                conv2b = vgg19_builder.Conv2d_layer(conv2a, filters=128)
+
+                pool2 = vgg19_builder.Pool_layer(conv2b)
+
+                conv3a = vgg19_builder.Conv2d_layer(pool2, filters=256)
+                conv3b = vgg19_builder.Conv2d_layer(conv3a, filters=256)
+                conv3c = vgg19_builder.Conv2d_layer(conv3b, filters=256)
+                conv3d = vgg19_builder.Conv2d_layer(conv3c, filters=256)
+
+                pool3 = vgg19_builder.Pool_layer(conv3d)
+
+                conv4a = vgg19_builder.Conv2d_layer(pool3, filters=512)
+                conv4b = vgg19_builder.Conv2d_layer(conv4a, filters=512)
+                conv4c = vgg19_builder.Conv2d_layer(conv4b, filters=512)
+                conv4d = vgg19_builder.Conv2d_layer(conv4c, filters=512)
+
+                pool4 = vgg19_builder.Pool_layer(conv4d)
+
+                conv5a = vgg19_builder.Conv2d_layer(pool4, filters=512)
+                conv5b = vgg19_builder.Conv2d_layer(conv5a, filters=512)
+                conv5c = vgg19_builder.Conv2d_layer(conv5b, filters=512)
+                conv5d = vgg19_builder.Conv2d_layer(conv5c, filters=512)
+
+                pool5 = vgg19_builder.Pool_layer(conv5d)
+
+                #DENSELY CONNECTED
+                fc1 = vgg19_builder.FC_layer(pool5, filters=4096)
+                drop1 = vgg19_builder.Dropout_layer(fc1)
+
+                fc2 = vgg19_builder.FC_layer(drop1, filters=4096)
+                drop2 = vgg19_builder.Dropout_layer(fc2)
+
+                output = vgg19_builder.FC_layer(drop2, filters=self.kwargs['Classes'], readout=True)
+
+                VGG19_dict = {'Input_ph': input_placeholder, 'Output_ph': output_placeholder, 'Output': output, 'Dropout_prob_ph': dropout_prob_placeholder }
+                return(VGG19_dict)
+
+
+
+
