6e55c67fa45a3b132bf92cf564fa0e871a2fddef
Dhruv Mohan <Dhruv.Mohan@outlook.com>
2017-11-15 21:00:28 +0530

    Image to sequence dataset generation init commit

---
+++ Super_TF/Dataset_IO/ImageSeqGen/Dataset_config_ImageSeqGen.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+
+class Dataset_conifg_ImageSeqGen(object):
+    """Shared config for ImageSeqGen Dataset """
+
+    def __init__(self):
+        self._Image_handle     = 'image_raw'
+        self._Seq_handle     = 'sequence'
+        self._Seg_mask        = 'seq_mask'
+        self._Height_handle    = 'height'
+        self._Width_handle     = 'width'
+        self._Depth_handle     = 'depth'
+        self._Image_name       = 'image_name'
+        self._Feature_dict     =  {\
+            self._Seq_handle  : tf.FixedLenFeature([], tf.string),\
+            self._Seq_mask  : tf.FixedLenFeature([], tf.string),\
+            self._Image_handle  : tf.FixedLenFeature([], tf.string),\
+            self._Height_handle : tf.FixedLenFeature([], tf.int64),\
+            self._Width_handle  : tf.FixedLenFeature([], tf.int64),\
+            self._Depth_handle  : tf.FixedLenFeature([], tf.int64),\
+            self._Image_name    : tf.FixedLenFeature([], tf.string)}
--- Super_TF/Dataset_IO/ImageSeqGen/Dataset_writer_ImageSeqGen.py
+++ Super_TF/Dataset_IO/ImageSeqGen/Dataset_writer_ImageSeqGen.py
@@ -1,41 +1,60 @@
 from utils.Dataset_writer import Dataset_writer
-from Dataset_IO.Classification.Dataset_conifg_classification import Dataset_conifg_classification
-import Dataset_IO.Classification.Dataset_classification_pb2 as proto
+from Dataset_IO.ImageSeqGen.Dataset_config_ImageSeqGen import Dataset_conifg_ImageSeqGen
+import Dataset_IO.ImageSeqGen.Dataset_ImageSeqGen_pb2
 import tensorflow as tf
 import os
 import random
-
 
 
 class label_helper(object):
 
     def __init__(self, image_path=None, image_data=None):
         self.image_path=image_path
-        self.image_data=image_data
-        _, self.image_name=os.path.split(image_path)
+        self.pad_generate_mask(image_data)
 
 
-class Dataset_writer_classification(Dataset_conifg_classification,Dataset_writer):
-    """Implementation of Dataset writer for classification"""
+    def pad_generate_mask(self, seq, max_seq_length=13):
+        start_seq='$' + seq
+        final_seq=start_seq + '#'
+        pad_length = max_seq_length - len(final_seq)
+        mask=''
+        for _ in range(pad_length):
+            final_seq = final_seq + '-'
+        for char in final_seq:
+            if char is not '-':
+                mask=mask+'1'
+            else:
+                mask=mask+'0'
+        self.seq_mask = mask
+        self.image_data = final_seq
+
+
+
+class Dataset_writer_ImageSeqGen(Dataset_conifg_ImageSeqGen,Dataset_writer):
+    """Implementation of Dataset writer for ImageSeqGen"""
 
     def __init__(self, Dataset_filename, image_shape=[]):
         self.dataset_name, _ =  os.path.splitext(Dataset_filename)
-        with tf.name_scope('Dataset_Classification_Writer') as scope:
+        with tf.name_scope('Dataset_ImageSeqGen_Writer') as scope:
             self.image_shape = image_shape
             super().__init__()
             self.construct_writer(Dataset_filename)
+
             self.Param_dict = {\
                 self._Height_handle : self._int64_feature(image_shape[0]),\
                 self._Width_handle  : self._int64_feature(image_shape[1]),\
                 self._Depth_handle  : self._int64_feature(image_shape[2]),\
-                self._Label_handle  : None,\
+                self._Seq_handle  : None,\
+                self._Seq_mask  : None,\
                 self._Image_handle  : None,\
                 self._Image_name    : None}
+
             self.mean_header_proto = proto.Image_set()
             self.mean_header_proto.Image_headers.image_width = image_shape[1]
             self.mean_header_proto.Image_headers.image_height = image_shape[0]
             self.mean_header_proto.Image_headers.image_depth = image_shape[2]
             self.dataset_mean = tf.Variable(tf.zeros(shape=[self.image_shape[0], self.image_shape[1], self.image_shape[2]]))
+
     def prune_data(self, image):
         _, ext = os.path.splitext(image)
         if ext in ['.jpg', '.png']:
@@ -43,15 +62,16 @@ class Dataset_writer_classification(Dataset_conifg_classification,Dataset_writer
         else:
             return False
 
-
-
     def __shuffle_input(self, image_list):
-        self.shuffled_images = []
-        for im_lab, im_paths in enumerate(image_list):
-            for im_path in im_paths:
-                self.shuffled_images.extend([label_helper(image_path=im_path, image_data=im_lab)])
+        data_contianer = []
+        for data_val in image_list:
+            data_contianer.extend([label_helper(image_path=data_val[0], image_data=data_val[1])])
+        self.shuffled_images = image_list
         random.shuffle(self.shuffled_images)
 
+    def __file_dict_constructor(self,  file_seq_list):
+        image_list = file_seq_list
+        self.__shuffle_input(image_list)
 
     def __filepath_constructor(self, filename_path):
         image_list = []
@@ -64,26 +84,11 @@ class Dataset_writer_classification(Dataset_conifg_classification,Dataset_writer
                     image_list.append(images_full_path)
         self.__shuffle_input(image_list)
 
-
-    def __file_dict_constructor(self, file_dict):
-        image_list = []
-        for key in sorted(file_dict):
-            image_list.append(file_dict[key])
-        self.__shuffle_input(image_list)
-
-
-    def filename_constructor(self, filename_path = None, file_dict= None):
-        if filename_path is not None:
-            self.__filepath_constructor(filename_path)
-        elif file_dict is not None:
+    def filename_constructor(self, file_dict= None):
             self.__file_dict_constructor(file_dict)
 
-
-
-
-
     def write_record(self, sess=None):
-        with tf.name_scope('Dataset_Classification_Writer') as scope:
+        with tf.name_scope('Dataset_ImageSeqGen_Writer') as scope:
             if sess is None:
                 self.sess = tf.get_default_session()
             else:
@@ -100,7 +105,8 @@ class Dataset_writer_classification(Dataset_conifg_classification,Dataset_writer
             for index , image_container in enumerate(self.shuffled_images):
                 printProgressBar(index+1, total_images)
                 im_rw = self.sess.run([image_raw, mean_assign],feed_dict={im_pth: image_container.image_path})
-                self.Param_dict[self._Label_handle] = self._int64_feature(image_container.image_data)
+                self.Param_dict[self._Seq_handle] = self._bytes_feature(image_container.image_data)
+                self.Param_dict[self._Seq_mask] = self._bytes_feature(image_container.seq_mask)
                 self.Param_dict[self._Image_handle] = self._bytes_feature(im_rw[0])
                 self.Param_dict[self._Image_name]   = self._bytes_feature(str.encode(image_container.image_name))
                 example = tf.train.Example(features=tf.train.Features(feature=self.Param_dict))
@@ -122,6 +128,7 @@ class Dataset_writer_classification(Dataset_conifg_classification,Dataset_writer
 
 
 #From: https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+
 
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 50, fill = '█'):
     """
