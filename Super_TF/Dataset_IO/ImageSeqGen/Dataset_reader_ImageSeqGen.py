ae1a143fb660ab7b7d5ba5fbd945903551d6db68
Dhruv Mohan <Dhruv.Mohan@outlook.com>
2017-11-27 16:26:21 +0530

    forward and reverse vocab dictionary and sequence decoding

--- Super_TF/Dataset_IO/ImageSeqGen/Dataset_reader_ImageSeqGen.py
+++ Super_TF/Dataset_IO/ImageSeqGen/Dataset_reader_ImageSeqGen.py
@@ -8,11 +8,23 @@ import random
 
 class Dataset_reader_ImageSeqGen(Dataset_reader, Dataset_conifg_ImageSeqGen):
 
-    def __init__(self, filename=None, epochs=100, vocab=40):
+    def __init__(self, filename=None, epochs=100, vocab=None):
         super().__init__()
         with tf.name_scope('Dataset_ImageSeqGen_Reader') as scope:
             self.batch_size = tf.placeholder(tf.int32, name='Dataset_batch_size')
-            self.num_vocab = vocab
+
+            self.fwd_dict = {}
+            self.rev_dict = {}
+            if vocab is None:
+                self.vocab = [['0', 0], ['1', 1], ['2', 2], ['3', 3], ['4', 4], ['5', 5], ['6', 6], ['7', 7], ['8', 8], ['9', 9],\
+                    ['A', 10], ['B', 11], ['C', 12], ['D', 13], ['E', 14], ['F', 15], ['G', 16], ['H', 17], ['I', 18], ['J', 19], ['K', 20], ['L', 21],\
+                    ['M', 22], ['N', 23], ['O', 24], ['P', 25], ['Q', 26], ['R', 27], ['S', 28], ['T', 29], ['U', 30], ['V', 31], ['W', 32], ['X', 33],\
+                    ['Y', 34], ['Z', 35], ['$', 36], ['#', 37], ['-', 38]]
+            else:
+                self.vocab = vocab
+
+            self.const_vocab_dict()
+
             self.open_dataset(filename=filename, epochs=epochs)
             self.mean_header_proto = proto.Image_set()
             dataset_path, dataset_name = os.path.split(filename)
@@ -43,10 +55,14 @@ class Dataset_reader_ImageSeqGen(Dataset_reader, Dataset_conifg_ImageSeqGen):
 
             complete_seq = features[self._Seq_handle]
             complete_mask = features[self._Seq_mask]
+
+            decoded_seq = get_seq(complete_seq.upper())
+            decoded_mask = get_seq(complete_mask)
+
             sequence_lenght = len(complete_seq)
-            input_seq = complete_seq[0:sequence_lenght-1]
-            target_seq = complete_seq[1:sequence_lenght]
-            final_mask = complete_mask[0:sequence_lenght-1]
+            input_seq = decoded_seq[0:sequence_lenght-1]
+            target_seq = decoded_seq[1:sequence_lenght]
+            final_mask = decoded_mask[0:sequence_lenght-1]
             return images, input_seq, target_seq, final_mask
 
 
@@ -55,3 +71,14 @@ class Dataset_reader_ImageSeqGen(Dataset_reader, Dataset_conifg_ImageSeqGen):
         images , out_input_seq, out_target_seq, out_final_mask = tf.train.shuffle_batch([image , input_seq, target_seq, final_mask], batch_size=self.batch_size, num_threads=8, capacity=5000+128, min_after_dequeue=5000)
         #TODO: seq encoding via vocab dict
         return images, out_input_seq, out_target_seq, out_final_mask
+
+    def const_vocab_dict(self):
+        for element in self.vocab:
+            self.fwd_dict[element[0]] = element[1]
+            self.rev_dict[element[1]] = element[0]
+
+    def get_seq(sequence):
+        output_seq = []
+        for char in sequence:
+            output_seq.append(self.fwd_dict[char])
+        return output_seq
