ebba0c11358c0d285d7fb94697e16b0a797c2d4d
Dhruv Mohan <Dhruv.Mohan@outlook.com>
2018-01-29 15:05:02 +0530

    Updating Stargan skeleton

--- Super_TF/Model_builder/Architecture/Gan/Stargan.py
+++ Super_TF/Model_builder/Architecture/Gan/Stargan.py
@@ -8,17 +8,13 @@ def Build_Stargan(kwargs):
     '''
     with tf.name_scope('Stargan_generator'):
         with Builder(**kwargs) as stargen_builder:
-                input_placeholder = tf.placeholder(tf.float32, \
-                    shape=[None, kwargs['Image_width']*kwargs['Image_height']*kwargs['Image_cspace']], name='Input')
-                output_placeholder = tf.placeholder(tf.float32, \
-                    shape=[None, kwargs['Image_width']*kwargs['Image_height']], name='Mask')
-                weight_placeholder = tf.placeholder(tf.float32, \
-                    shape=[None, kwargs['Image_width']*kwargs['Image_height']], name='Weight')
-                dropout_prob_placeholder = tf.placeholder(tf.float32, name='Dropout')
-                state_placeholder = tf.placeholder(tf.string, name="State")
-                input_reshape = stargen_builder.Reshape_input(input_placeholder, \
-                    width=kwargs['Image_width'], height=kwargs['Image_height'], colorspace= kwargs['Image_cspace'])
-                stargen_builder.control_params(Dropout_control=dropout_prob_placeholder, State=state_placeholder)
+                gen_input_placeholder = tf.placeholder(tf.float32, \
+                    shape=[None, kwargs['Image_width'], kwargs['Image_height'], kwargs['Image_cspace']], name='Input')
+                gen_output_placeholder = tf.placeholder(tf.float32, \
+                    shape=[None, kwargs['Image_width'], kwargs['Image_height']], name='Mask')
+                gen_dropout_prob_placeholder = tf.placeholder(tf.float32, name='Dropout')
+                gen_state_placeholder = tf.placeholder(tf.string, name="State")
+                stargen_builder.control_params(Dropout_control = gen_dropout_prob_placeholder, State = gen_state_placeholder))
                 
                 def residual_unit(input, filters=256):
                     Conv_rl1 = stargen_builder.Conv2d_layer(input, k_size=[3, 3], stride=[1, 2, 2, 1], Batch_norm=True, filters=filters)
@@ -27,7 +23,7 @@ def Build_Stargan(kwargs):
                     return Res_connect
 
                 with tf.name_scope('Downsample'):
-                    Conv1 = stargen_builder.Conv2d_layer(input_reshape, k_size=[7, 7], Batch_norm=True, filters=64)
+                    Conv1 = stargen_builder.Conv2d_layer(gen_input_placeholder, k_size=[7, 7], Batch_norm=True, filters=64)
                     Conv2 = stargen_builder.Conv2d_layer(Conv1, k_size=[4, 4], stride=[1, 2, 2, 1], Batch_norm=True, filters=128, padding='VALID')
                     Conv3 = stargen_builder.Conv2d_layer(Conv2, k_size=[4, 4], stride=[1, 2, 2, 1], Batch_norm=True, filters=256, padding='VALID')
 
@@ -41,30 +37,36 @@ def Build_Stargan(kwargs):
                     Upconv1 = stargen_builder.Conv_Resize_layer(Res_bl3, filters=128, Batch_norm=True, Activation=True)
                     Upconv2 = stargen_builder.Conv_Resize_layer(Upconv2, filters=64, Batch_norm=True, Activation=True)
                     Final_conv = stargen_builder.Conv2d_layer(Conv2, k_size=[7, 7], filters=3, Activation=False)
-                    Output = stargen_builder.Activation(Final_conv, Type='TANH')
+                    Output_gen = stargen_builder.Activation(Final_conv, Type='TANH')
 
 
         with Builder(**kwargs) as stardis_builder:
-                input_placeholder = tf.placeholder(tf.float32, \
-                    shape=[None, kwargs['Image_width']*kwargs['Image_height']*kwargs['Image_cspace']], name='Input')
-                output_placeholder = tf.placeholder(tf.float32, \
-                    shape=[None, kwargs['Image_width']*kwargs['Image_height']], name='Mask')
-                weight_placeholder = tf.placeholder(tf.float32, \
-                    shape=[None, kwargs['Image_width']*kwargs['Image_height']], name='Weight')
-                dropout_prob_placeholder = tf.placeholder(tf.float32, name='Dropout')
-                state_placeholder = tf.placeholder(tf.string, name="State")
-                input_reshape = stardis_builder.Reshape_input(input_placeholder, \
-                    width=kwargs['Image_width'], height=kwargs['Image_height'], colorspace= kwargs['Image_cspace'])
-                stardis_builder.control_params(Dropout_control=dropout_prob_placeholder, State=state_placeholder)
+                dis_input_placeholder = tf.placeholder(tf.float32, \
+                    shape=[None, kwargs['Image_width'], kwargs['Image_height'], kwargs['Image_cspace']], name='Input')
+                dis_output_placeholder = tf.placeholder(tf.float32, \
+                    shape=[None, kwargs['Image_width'], kwargs['Image_height']], name='Mask')
+                dis_dropout_prob_placeholder = tf.placeholder(tf.float32, name='Dropout')
+                dis_state_placeholder = tf.placeholder(tf.string, name="State")
 
-                Dis_conv1 = stardis_builder.Conv2d_layer(input_reshape, k_size=[4, 4], stride=[1, 2, 2, 1], filters=64, padding='VALID')
-                Dis_conv2 = stardis_builder.Conv2d_layer(Dis_conv1, k_size=[4, 4], stride=[1, 2, 2, 1], filters=128, padding='VALID')
-                Dis_conv3 = stardis_builder.Conv2d_layer(Dis_conv2, k_size=[4, 4], stride=[1, 2, 2, 1], filters=256, padding='VALID')
-                Dis_conv4 = stardis_builder.Conv2d_layer(Dis_conv3, k_size=[4, 4], stride=[1, 2, 2, 1], filters=512, padding='VALID')
-                Dis_conv5 = stardis_builder.Conv2d_layer(Dis_conv4, k_size=[4, 4], stride=[1, 2, 2, 1], filters=1024, padding='VALID')
-                Dis_conv6 = stardis_builder.Conv2d_layer(Dis_conv5, k_size=[4, 4], stride=[1, 2, 2, 1], filters=2048, padding='VALID')
+                stardis_builder.control_params(Dropout_control=dis_dropout_prob_placeholder, State=dis_state_placeholder)
+
+                Dis_conv1 = stardis_builder.Conv2d_layer(dis_input_placeholder, k_size=[4, 4], stride=[1, 2, 2, 1], filters=64, padding='VALID', Activation=False)
+                Dis_conv1 = stardis_builder.Activation(input, Type='LRELU')
+                Dis_conv2 = stardis_builder.Conv2d_layer(Dis_conv1, k_size=[4, 4], stride=[1, 2, 2, 1], filters=128, padding='VALID', Activation=False)
+                Dis_conv2 = stardis_builder.Activation(Dis_conv2, Type='LRELU')
+                Dis_conv3 = stardis_builder.Conv2d_layer(Dis_conv2, k_size=[4, 4], stride=[1, 2, 2, 1], filters=256, padding='VALID', Activation=False)
+                Dis_conv3 = stardis_builder.Activation(Dis_conv3, Type='LRELU')
+                Dis_conv4 = stardis_builder.Conv2d_layer(Dis_conv3, k_size=[4, 4], stride=[1, 2, 2, 1], filters=512, padding='VALID', Activation=False)
+                Dis_conv4 = stardis_builder.Activation(Dis_conv4, Type='LRELU')
+                Dis_conv5 = stardis_builder.Conv2d_layer(Dis_conv4, k_size=[4, 4], stride=[1, 2, 2, 1], filters=1024, padding='VALID', Activation=False)
+                Dis_conv5 = stardis_builder.Activation(Dis_conv5, Type='LRELU')
+                Dis_conv6 = stardis_builder.Conv2d_layer(Dis_conv5, k_size=[4, 4], stride=[1, 2, 2, 1], filters=2048, padding='VALID', Activation=False)
+                Dis_conv6 = stardis_builder.Activation(Dis_conv6, Type='LRELU')
 
                 Output_Dsrc = stardis_builder.Conv2d_layer(Dis_conv6, k_size=[3, 3], stride=[1, 2, 2, 1], filters=1, Activation=False)
                 Output_Dcls = stardis_builder.Conv2d_layer(Dis_conv6, k_size=[kwargs['Image_height']/64, kwargs['Image_width']/64], Activation=False)
+
+
+        return "GAN"
 
 
