Fix incorrect WBCE loss calculation in Pnet

Super_TF/Model_builder/Architecture/Segmentation/Pnet.py
@@ -68,12 +68,14 @@ class Pnet(Base_Segnet):
     
     def construct_loss(self):
         super().construct_loss()
-        Probs = tf.nn.sigmoid(self.output)
+        output = tf.reshape(self.output, shape=(-1, self.build_params['Image_width'] * self.build_params['Image_height']))
+        output_placeholder = tf.reshape(self.output_placeholder, shape=(-1, self.build_params['Image_width'] * self.build_params['Image_height']))
+        Probs = tf.nn.sigmoid(output)
         offset = 1e-5
         Threshold = 0.1
         Probs_processed = tf.clip_by_value(Probs, offset, 1.0)
         Con_Probs_processed = tf.clip_by_value(1-Probs, offset, 1.0)
-        W_I = (-self.output_placeholder * tf.log(Probs_processed) - (1 - self.output_placeholder)*tf.log(Con_Probs_processed))
+        W_I = (-output_placeholder * tf.log(Probs_processed) - (1 - output_placeholder)*tf.log(Con_Probs_processed))
         Weighted_BCE_loss = tf.reduce_sum(W_I) / tf.cast(tf.maximum(tf.count_nonzero(W_I -Threshold),0), tf.float32)
         tf.summary.scalar('WBCE loss', Weighted_BCE_loss)
         tf.summary.image('WCBE', tf.reshape(W_I, [-1, self.build_params['Image_width'], self.build_params['Image_height'], 1]))